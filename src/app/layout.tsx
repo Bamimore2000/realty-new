dbee199f9bc184284cbb8d9bd1efe47bad129389
sogzy <emmanuelar35@gmail.com>
2025-09-01 00:52:04 +0100

    added some

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 import Navbar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 
@@ -86,6 +87,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics />
         <Navbar />
         <main className="pt-14">{children}</main>
         <Footer />
